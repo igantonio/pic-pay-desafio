0f3323f830abae26f38af1f70654ea5e8369a259
Igor Gabriel <igor.gabriel.saturnino@gmail.com>
2024-08-28 19:29:54 -0300

    Init project

---
+++ src/main/kotlin/com/example/pic_pay_desafio/PicPayDesafioApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pic_pay_desafio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PicPayDesafioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PicPayDesafioApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/pic_pay_desafio/PicPayDesafioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pic_pay_desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PicPayDesafioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
